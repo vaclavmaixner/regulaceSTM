29d5d1ecf681145d0f95210dfd03990ee6e675bf
Vaclav Maixner <vaclavmaixnerj@gmail.com>
2016-09-05 20:30:32 +0200

    s

--- src/regulaceSTM/Main.java
+++ src/regulaceSTM/Main.java
@@ -1,14 +1,36 @@
 package regulaceSTM;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Main {
-	public static void main(String[] args) {
-		// final double kp = 10; // konstanty proporcni, integracni, derivacni
-		// final double ki = 10;
-		// final double kd = 10;
+	public static void main(String[] args) throws IOException {
+		final double kp = 4; // konstanty proporcni, integracni, derivacni
+		final double ki = 3;
+		final double kd = 0;
 
 		Pid pid1 = new Pid();
-		for (int i = 0; i < 400; i++) {
-			System.out.println(pid1.solve(0.2, 0.2, 0, 6));
+		double input = 7;
+
+		// DataOutputStream dataOut = new DataOutputStream(new FileOutputStream(
+		// "test.txt"));
+
+		PrintWriter out = new PrintWriter("results2.txt");
+
+		for (int i = 1; i <= 200; i++) {
+			System.out.print(i + " "); // vytiskne pro gnuplot poradnik
+			System.out.println(pid1.solve(kp, ki, kd, input));
+
+			// prevede neobratne output
+			Double output2 = (pid1.solve(kp, ki, kd, input));
+			String output3 = Double.toString(output2);
+			// print to file
+			out.println(i + " " + output3);
+
+			// znovu vola sebe samu s outputem jako novou, aktualni hodnotou,
+			// tedy inputem
+			input = pid1.solve(kp, ki, kd, input);
 		}
+		out.close();
 	}
 }
