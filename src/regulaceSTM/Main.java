f324ed4e90e0a9f3bc4956d3e25b9bfff9caabaa
Vaclav Maixner <vaclavmaixnerj@gmail.com>
2016-09-09 14:38:15 +0200

    Prechod na pixely a zubaty signaly. Fuj.

--- src/regulaceSTM/Main.java
+++ src/regulaceSTM/Main.java
@@ -11,9 +11,15 @@ public class Main {
 		final double ki = 4;
 		final double kd = 0;
 		final double kc = 1;
-		double numberOfSteps = 2000; // pocet kroku
-		int speed = 1;
+		int iterations = 20;
+		double pixels = 512;
+		double numberOfSteps = iterations * pixels; // pocet kroku
+		double moleculeHeight = 0.1;
+
 		final boolean overlap = true;
+
+		boolean filter = false;
+		boolean average = false;
 
 		Pid pid1 = new Pid();
 
@@ -28,7 +34,8 @@ public class Main {
 		// double inputCurrent = 2; // pocet kroku
 		// double input = pid1.convertCurrentToZ(inputCurrent, kc);
 
-		String evaluateResult = null;
+		// *****
+		double averageOutputCounter = 0;
 
 		// vytvorit jmeno souboru do ktereho ulozime data, aby byly zpetne
 		// dohledatelne pocatecni podminky
@@ -58,29 +65,32 @@ public class Main {
 		// naopak
 
 		boolean moleculePresent = false;
-
+		int counter = 0;
 		// hlavni smycka
 		// pracujeme v nanometrech
-		for (int i = 1; i <= numberOfSteps; i += speed) {
+		for (int i = 1; i <= (numberOfSteps); i++) {
+
+			counter += 1;
+
 			// schod
-			if (i == (numberOfSteps / 100) * 40) {
+			if (i == Math.round((numberOfSteps / 100) * 40)) {
 				level += (1d / 3d);
 			}
 
-			if (i == ((numberOfSteps / 100) * 60)) {
+			if (i == Math.round((numberOfSteps / 100) * 60)) {
 				level -= (1d / 3d);
 			}
 
 			// korugace povrchu
-			double SetpointDistance = (((1d / 30d) * Math.sin((60 / 3.14) * i)) + level);
+			double SetpointDistance = (((1d / 30d) * Math.sin((240 / 3.14) * i)) + level);
 			// vyvyseni zpusobene molekulou
-			double SetpointDistanceWithMolecule = (((1d / 30d) * Math
-					.sin((0.01) * i)) + level) + 0.1;
+			double SetpointDistanceWithMolecule = level + moleculeHeight;
 			// ulozeni puvodni urovne povrchu
 			double SetpointDistanceWithoutMolecule = SetpointDistance;
 
 			// usek nahodneho vyskytu castice
-			if (i >= 700 && i < 800) {
+			if (i >= Math.round((numberOfSteps / 100) * 35)
+					&& i < Math.round((numberOfSteps / 100) * 40)) {
 				if (moleculePresent == false) {
 					Random randomGenerator = new Random();
 					int randomInt = randomGenerator.nextInt(8);
@@ -117,9 +127,6 @@ public class Main {
 			// System.out.println("Hodnoty jsou ****   " + pidOutput);
 			Double distance = pid1.convertCurrentToZ(pidOutput, kc);
 
-			// System.out.println("Hodnoty jsou ****   " + distance);
-
-			// *********
 			Double position = SetpointDistance + distance;
 
 			if (overlap) {
@@ -128,20 +135,37 @@ public class Main {
 
 			// vola evaluaci dat - oscilace a jine
 			Output output1 = new Output();
-			evaluateResult = output1.evaluateOutput(pidOutput, i, Setpoint);
+			// evaluateResult = output1.evaluateOutput(pidOutput, i, Setpoint);
 
-			// konzolovej output
-			System.out.print(i + " "); // vytiskne pro gnuplot poradnik
-			System.out.println((position) + " " + SetpointDistance);
+			if (filter == false && average == false) {
+				if ((counter) % (20) == 0) {
+					// konzolovej output
+					System.out.print(i + " "); // vytiskne pro gnuplot poradnik
+					System.out.println((position) + " " + SetpointDistance);
 
-			// print to file
-			out.println(i + " " + position + " " + SetpointDistance);
+					// print to file
+					out.println(i + " " + position + " " + SetpointDistance);
+
+				}
+			} else if (filter == false && average == true) {
+				averageOutputCounter += position;
+				if ((counter) % (20) == 0) {
+					position = averageOutputCounter / 20;
+					// konzolovej output
+					System.out.print(i + " "); // vytiskne pro gnuplot poradnik
+					System.out.println((position) + " " + SetpointDistance);
+
+					// print to file
+					out.println(i + " " + position + " " + SetpointDistance);
+
+					averageOutputCounter = 0;
+				}
+			} else if (filter == true && average == false) {
+				return;
+			}
 
 			// ulozi si predchozi output pro porovnani
 			// previousPidOutput = pidOutput;
-
-			// znovu vola sebe samu s outputem jako novou, aktualni hodnotou,
-			// tedy inputem
 
 			// if (randomInt != 0) {
 			// pidOutput -= fuzzy;
